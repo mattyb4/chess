933862fbca0dde2c4bcfe00cacc95b4a7e99c370
Matt Banks <mjbanks4@gmail.com>
2025-06-27 11:52:36 -0600

    Removed redundant method from PieceMovesCalc

--- shared/src/main/java/chess/PieceMovesCalc.java
+++ shared/src/main/java/chess/PieceMovesCalc.java
@@ -3,18 +3,7 @@ import java.util.Collection;
 import java.util.ArrayList;
 
 public interface PieceMovesCalc {
-    static Collection<ChessMove> calcMoves(ChessBoard board, ChessPosition startPosition, ArrayList<ChessMove> calcedMoves) {
-        Collection<ChessMove> moves = new ArrayList<>();
-        for (ChessMove move : calcedMoves) {
-            //System.out.println("in for loop");
-            //System.out.println(move);
-            ChessPosition start = move.getStartPosition();
-            ChessPosition end = move.getEndPosition();
-            ChessPiece.PieceType promotion = move.getPromotionPiece();
-            moves.add(new ChessMove(start,end,promotion));
-        }
-        //var possiblePosition = new ChessPosition(1,8);
-        //moves.add(new ChessMove(startPosition,possiblePosition,null));
-        return moves;
-    }
+
+    Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition startPosition);
+
 }
