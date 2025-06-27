682f2b485902e020e79a71971273e2aefea7bff9
Matt Banks <mjbanks4@gmail.com>
2025-06-27 17:32:56 -0600

    Fixed knight calcs by making them if statements instead of loops and adjusting boundaries

--- shared/src/main/java/chess/KnightMovesCalc.java
+++ shared/src/main/java/chess/KnightMovesCalc.java
@@ -15,145 +15,148 @@ public class KnightMovesCalc implements PieceMovesCalc {
 
 
         //move right, up two
-        while(newX > 0 && newX < 7 && newY > 0 && newY < 8) {
+        if(newX > 0 && newX < 7 && newY > 0 && newY < 8) {
             newX = newX + 2;
             newY = newY + 1;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving right, up two");
-            System.out.println(new ChessMove(currentPosition,newPosition,null));
+
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
         }
         newX = x;
         newY = y;
 
         //move up, right two
-        while(newX > 0 && newX < 8 && newY > 0 && newY < 7) {
+        if(newX > 0 && newX < 8 && newY > 0 && newY < 7) {
             newX = newX + 1;
             newY = newY + 2;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving up, right two");
-            System.out.println(new ChessMove(currentPosition,newPosition,null));
+
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
         }
         newX = x;
         newY = y;
 
         //move left, up two
-        while(newX > 0 && newX < 7 && newY > 0 && newY < 8) {
+        if(newX > 0 && newX < 7 && newY > 1 && newY <= 8) {
             newX = newX + 2;
             newY = newY - 1;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving left, up two");
-            System.out.println(new ChessMove(currentPosition,newPosition,null));
+
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+
         }
         newX = x;
         newY = y;
 
         //move up, left two
-        while(newX > 0 && newX < 8 && newY > 1 && newY < 8) {
+        if(newX > 0 && newX < 8 && newY > 1 && newY <= 8) {
             newX = newX + 1;
             newY = newY - 2;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving up, left two");
-            System.out.println(new ChessMove(currentPosition,newPosition,null));
+
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
         }
         newX = x;
         newY = y;
 
         //move down, right two
-        while(newX > 0 && newX < 8 && newY > 0 && newY < 7) {
+        if(newX > 1 && newX <= 8 && newY > 0 && newY < 7) {
             newX = newX - 1;
             newY = newY + 2;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving down, right two");
-            System.out.println(new ChessMove(currentPosition,newPosition,null));
+
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
         }
         newX = x;
         newY = y;
 
         //move right, down two
-        while(newX > 1 && newX < 8 && newY > 0 && newY < 8) {
+        if(newX > 2 && newX <= 8 && newY > 0 && newY < 8) {
             newX = newX - 2;
             newY = newY + 1;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving right, down two");
-            System.out.println(new ChessMove(currentPosition,newPosition,null));
+
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+
         }
         newX = x;
         newY = y;
 
         //move left, down two
-        while(newX > 1 && newX < 8 && newY > 0 && newY < 8) {
+        if(newX > 2 && newX <= 8 && newY > 1 && newY <= 8) {
             newX = newX - 2;
             newY = newY - 1;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving left, down two");
             System.out.println(new ChessMove(currentPosition,newPosition,null));
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+
         }
         newX = x;
         newY = y;
 
         //move down, left two
-        while(newX > 0 && newX < 8 && newY > 1 && newY < 8) {
+        if(newX > 1 && newX <= 8 && newY > 1 && newY <= 8) {
             newX = newX - 1;
             newY = newY - 2;
             var newPosition = new ChessPosition(newX,newY);
             System.out.println("moving down, left two");
-            System.out.println(new ChessMove(currentPosition,newPosition,null));
+
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
+                if(board.getPiece(newPosition).getTeamColor() != myPiece.getTeamColor()){
+                    knightMoves.add(new ChessMove(currentPosition,newPosition,null));
+                    System.out.println(new ChessMove(currentPosition,newPosition,null));
                 }
             }
-            knightMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            else knightMoves.add(new ChessMove(currentPosition,newPosition,null));
         }
         newX = x;
         newY = y;
