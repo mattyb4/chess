651311cadb03014b5847168abcb764b1864e9232
Matt Banks <mjbanks4@gmail.com>
2025-06-27 12:44:40 -0600

    Created RookMovesCalc

---
+++ shared/src/main/java/chess/RookMovesCalc.java
@@ -0,0 +1,12 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RookMovesCalc implements PieceMovesCalc {
+    @Override
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition currentPosition) {
+        ArrayList<ChessMove> rookMoves = new ArrayList<>();
+        return rookMoves;
+    }
+}
