25f8dbffa3fbbd64398ce1c07a2e288f871e2842
Matt Banks <mjbanks4@gmail.com>
2025-06-27 18:18:33 -0600

    started on pawn basic forward movement

--- shared/src/main/java/chess/PawnMovesCalc.java
+++ shared/src/main/java/chess/PawnMovesCalc.java
@@ -13,6 +13,64 @@ public class PawnMovesCalc implements PieceMovesCalc {
         int newY = y;
         var myPiece = board.getPiece(currentPosition);
 
+
+
+        //white move forward standard
+        if(newX > 0 && newX != 2 && newX < 8 && newY > 0 && newY < 8 && myPiece.getTeamColor() == ChessGame.TeamColor.WHITE){
+            newX = newX + 1;
+            var newPosition = new ChessPosition(newX,newY);
+            if(board.getPiece(newPosition) == null) {
+                pawnMoves.add(new ChessMove(currentPosition,newPosition,null));
+            }
+        }
+        newX = x;
+        newY = y;
+
+        //black move forward standard
+        if(newX > 0 && newX != 7 && newX < 8 && newY > 0 && newY < 8 && myPiece.getTeamColor() == ChessGame.TeamColor.BLACK){
+            newX = newX - 1;
+            var newPosition = new ChessPosition(newX,newY);
+            if(board.getPiece(newPosition) == null) {
+                pawnMoves.add(new ChessMove(currentPosition,newPosition,null));
+            }
+        }
+        newX = x;
+        newY = y;
+
+        //white move forward from starting position (can move up to two spaces)
+        if(newX == 2 && newY > 0 && newY < 8 && myPiece.getTeamColor() == ChessGame.TeamColor.WHITE){
+            newX = newX + 1;
+            var newPosition = new ChessPosition(newX,newY);
+            if(board.getPiece(newPosition) == null) {
+                pawnMoves.add(new ChessMove(currentPosition,newPosition,null));
+                //check if next space is also free
+                newX = newX + 1;
+                newPosition = new ChessPosition(newX,newY);
+                if(board.getPiece(newPosition) == null) {
+                    pawnMoves.add(new ChessMove(currentPosition,newPosition,null));
+                }
+            }
+        }
+        newX = x;
+        newY = y;
+
+        //black move forward from starting position (can move up to two spaces)
+        if(newX == 7 && newY > 0 && newY < 8 && myPiece.getTeamColor() == ChessGame.TeamColor.BLACK){
+            newX = newX - 1;
+            var newPosition = new ChessPosition(newX,newY);
+            if(board.getPiece(newPosition) == null) {
+                pawnMoves.add(new ChessMove(currentPosition,newPosition,null));
+                //check if next space is also free
+                newX = newX - 1;
+                newPosition = new ChessPosition(newX,newY);
+                if(board.getPiece(newPosition) == null) {
+                    pawnMoves.add(new ChessMove(currentPosition,newPosition,null));
+                }
+            }
+        }
+        newX = x;
+        newY = y;
+
         return pawnMoves;
     }
 }
