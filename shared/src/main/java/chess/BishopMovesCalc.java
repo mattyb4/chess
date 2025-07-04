6637465bb0f371b5bee98b50e4c7ff72fffdd479
Matt Banks <mjbanks4@gmail.com>
2025-07-04 12:27:39 -0600

    Cleaned up whitespace and variable names

--- shared/src/main/java/chess/BishopMovesCalc.java
+++ shared/src/main/java/chess/BishopMovesCalc.java
@@ -6,75 +6,78 @@ import java.util.ArrayList;
 public class BishopMovesCalc implements PieceMovesCalc {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition currentPosition) {
-        int x = currentPosition.getRow();
-        int y = currentPosition.getColumn();
+        int row = currentPosition.getRow();
+        int col = currentPosition.getColumn();
         ArrayList<ChessMove> bishopMoves = new ArrayList<>();
-        int newX = x;
-        int newY = y;
+        int newRow = row;
+        int newCol = col;
         var myPiece = board.getPiece(currentPosition);
-
         //move up and to the right
-        while (newX > 0 && newX < 8 && newY > 0 && newY < 8) {
-            newX = newX+1;
-            newY = newY+1;
-            var newPosition = new ChessPosition(newX,newY);
+        while (newRow < 8 && newCol < 8) {
+            newRow++;
+            newCol++;
+            var newPosition = new ChessPosition(newRow, newCol);
             if(board.getPiece(newPosition) != null) {//check for obstacle piece
                 if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
                     break;//stop short of own piece
                 }
             }
             bishopMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;//capture piece
-        }
-        newX = x; //reset to original position
-        newY = y;
-
-        //move up and to the left
-        while (newX > 1 && newX <= 8 && newY > 0 && newY < 8) {
-            newX = newX - 1;
-            newY = newY + 1;
-            var newPosition = new ChessPosition(newX,newY);
             if(board.getPiece(newPosition) != null) {
-                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
-                    break;
-                }
+                break;//capture piece
             }
-            bishopMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
         }
-        newX = x;
-        newY = y;
-
+        newRow = row; //reset to original position
+        newCol = col;
         //move down and to the right
-        while (newX > 0 && newX < 8 && newY > 1 && newY <= 8) {
-            newX = newX + 1;
-            newY = newY - 1;
-            var newPosition = new ChessPosition(newX,newY);
+        while (newRow > 1 && newCol < 8) {
+            newRow--;
+            newCol++;
+            var newPosition = new ChessPosition(newRow, newCol);
             if(board.getPiece(newPosition) != null) {
                 if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
                     break;
                 }
             }
             bishopMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            if(board.getPiece(newPosition) != null) {
+                break;//capture piece
+            }
         }
-        newX = x;
-        newY = y;
-
+        newRow = row;
+        newCol = col;
+        //move up and to the left
+        while (newRow < 8 && newCol > 1) {
+            newRow++;
+            newCol--;
+            var newPosition = new ChessPosition(newRow, newCol);
+            if(board.getPiece(newPosition) != null) {
+                if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
+                    break;
+                }
+            }
+            bishopMoves.add(new ChessMove(currentPosition,newPosition,null));
+            if(board.getPiece(newPosition) != null) {
+                break;//capture piece
+            }
+        }
+        newRow = row;
+        newCol = col;
         //move down and to the left
-        while (newX > 1 && newX <= 8 && newY > 1 && newY <= 8) {
-            newX = newX - 1;
-            newY = newY - 1;
-            var newPosition = new ChessPosition(newX,newY);
+        while (newRow > 1 && newCol > 1) {
+            newRow--;
+            newCol--;
+            var newPosition = new ChessPosition(newRow, newCol);
             if(board.getPiece(newPosition) != null) {
                 if(board.getPiece(newPosition).getTeamColor() == myPiece.getTeamColor()){
                     break;
                 }
             }
             bishopMoves.add(new ChessMove(currentPosition,newPosition,null));
-            if(board.getPiece(newPosition) != null) break;
+            if(board.getPiece(newPosition) != null) {
+                break;//capture piece
+            }
         }
-
         return bishopMoves;
     }
 }
