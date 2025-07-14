9d61db8e8e2c5bd5302129c8e3ae8751e4210638
Matt Banks <mjbanks4@gmail.com>
2025-07-14 17:23:03 -0600

    initialized all required methods in DAO interfaces

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,14 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.Collection;
+
 public interface GameDAO {
+    void clear() throws DataAccessException;
+    GameData createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+    GameData getUsername(String playerColor) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,5 +1,10 @@
 package dataaccess;
 
-public interface UserDAO {
+import model.UserData;
 
+public interface UserDAO {
+    void clear() throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    UserData getUserInfo(String username, String password) throws DataAccessException;
 }
