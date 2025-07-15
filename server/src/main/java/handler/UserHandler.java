e14f9800fe4b5fcfe7d809bfa16d779c23fdd20a
Matt Banks <mjbanks4@gmail.com>
2025-07-14 20:01:08 -0600

    Further updated register method. Potentially implemented pending testing

--- server/src/main/java/handler/UserHandler.java
+++ server/src/main/java/handler/UserHandler.java
@@ -1,6 +1,8 @@
 package handler;
 
 import com.google.gson.Gson;
+import dataaccess.AlreadyTakenException;
+import dataaccess.DataAccessException;
 import model.AuthData;
 import model.UserData;
 import service.UserService;
@@ -9,10 +11,12 @@ import spark.Response;
 
 public class UserHandler {
     UserService service;
-    public String register(Request req, Response res) {
+    public String register(Request req, Response res) throws DataAccessException, AlreadyTakenException {
         var serializer = new Gson();
         UserData data = serializer.fromJson(req.body(), UserData.class);
-        var registerRequest = new RegisterRequest(data.username(),data.password(),data.email());
-        AuthData authData =
+        AuthData authData = service.register(data);
+        var result = new Gson().toJson(authData);
+        res.status(200);
+        return result;
     }
 }
