f98a862a79a15c5b7526edd634d79fd78e33d5d5
Matt Banks <mjbanks4@gmail.com>
2025-07-22 10:15:27 -0600

    Implemented unit tests by converting former tests to use SQL DataAccess instead

---
+++ server/src/test/java/dataaccess/SQLClearTest.java
@@ -0,0 +1,53 @@
+package dataaccess;
+
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import service.GameService;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class SQLClearTest {
+    private SQLUserDAO userDAO;
+    private SQLAuthDAO authDAO;
+    private SQLGameDAO gameDAO;
+    private UserService userService;
+    private GameService gameService;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        userDAO = new SQLUserDAO();
+        authDAO = new SQLAuthDAO();
+        gameDAO = new SQLGameDAO();
+        userService = new UserService(userDAO, authDAO);
+        gameService = new GameService(gameDAO, authDAO);
+
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+    }
+
+    @Test
+    @DisplayName("Functioning clear method")
+    public void positiveClearTest() throws DataAccessException, BadRequestException, InvalidUserException {
+        userDAO.createUser(new UserData("username","password", "test@gmail.com"));
+        authDAO.createAuth(new AuthData("authtoken","username"));
+        gameService.create("gamename","authtoken");
+
+        assertNotNull(userDAO.getUser("username"));
+        assertNotNull(authDAO.getAuth("authtoken"));
+        assertNotNull(gameDAO.getGame(1));
+
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+
+        assertNull(userDAO.getUser("username"));
+        assertNull(authDAO.getAuth("authtoken"));
+        assertNull(gameDAO.getGame(1));
+    }
+}
---
+++ server/src/test/java/dataaccess/SQLUserTests.java
@@ -0,0 +1,79 @@
+package dataaccess;
+
+
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SQLUserTests {
+    private SQLUserDAO userDAO;
+    private SQLAuthDAO authDAO;
+    private UserService userService;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        userDAO = new SQLUserDAO();
+        authDAO = new SQLAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+
+        userDAO.clear();
+        authDAO.clear();
+    }
+
+    @Test
+    public void positiveRegisterTest() throws DataAccessException, BadRequestException, AlreadyTakenException {
+        var testUser = new UserData("username", "password", "test@gmail.com");
+        var authTest = userService.register(testUser);
+
+        assertNotNull(authTest.authToken());
+        assertEquals("username", authTest.username());
+    }
+    @Test
+    @DisplayName("Register failure - duplicate username")
+    public void negativeRegisterTest() throws DataAccessException, BadRequestException, AlreadyTakenException {
+        var testUser = new UserData("username", "password", "test@gmail.com");
+        userService.register(testUser);
+        assertThrows(AlreadyTakenException.class, () -> {userService.register(testUser);});
+    }
+
+    @Test
+    @DisplayName("Login Success")
+    public void positiveLoginTest() throws DataAccessException, BadRequestException, InvalidUserException, AlreadyTakenException {
+        var testUser = new UserData("username", "password", "test@gmail.com");
+        userService.register(testUser);
+        var authTest = userService.login(testUser);
+        assertNotNull(authTest.authToken());
+        assertEquals("username", authTest.username());
+    }
+
+    @Test
+    @DisplayName("Login failure")
+    public void negativeLoginTest() throws DataAccessException, BadRequestException, AlreadyTakenException {
+        var testUser = new UserData("username", "password", "test@gmail.com");
+        userService.register(testUser);
+        var wrongUser = new UserData("username", "wrongpass", "test@gmail.com");
+        assertThrows(InvalidUserException.class, () -> {userService.login(wrongUser);});
+    }
+
+    @Test
+    @DisplayName("Logout success")
+    public void positiveLogoutTest() throws DataAccessException, BadRequestException, AlreadyTakenException, InvalidUserException {
+        var testUser = new UserData("username", "password", "test@gmail.com");
+        var authTest = userService.register(testUser);
+        userService.logout(authTest.authToken());
+        assertNull(authDAO.getAuth(authTest.authToken()));
+    }
+
+    @Test
+    @DisplayName("Logout failure")
+    public void negativeLogoutTest() throws DataAccessException {
+        assertThrows(InvalidUserException.class, () -> {userService.logout("");});
+    }
+
+}
